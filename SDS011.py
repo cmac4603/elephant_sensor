6f55b1e30ddf6df5fe7615094625f3ac64ebd525
Colin MacRae <colin.macrae@hearst.co.uk>
2016-10-30 13:22:16 +0000

    main program with sds011 on lcd

--- SDS011.py
+++ SDS011.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-# coding=utf-8
-
 from __future__ import print_function
 import serial, struct
 
@@ -13,16 +11,18 @@ ser.flushInput()
 
 byte, data = 0, ""
 
-def process_frame(d):
-    r = struct.unpack('<HHxxBBB', d[2:])
-    pm25 = r[0]/10.0
-    pm10 = r[1]/10.0
-    checksum = sum(ord(v) for v in d[2:8])%256
-    print("PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
 
-while True:
-    while byte != "\xaa":
-        byte = ser.read(size=1)
-    d = ser.read(size=10)
-    if d[0] == "\xc0":
-        process_frame(byte + d)
+class SDS011():
+    def process_frame(self, d):
+        r = struct.unpack('<HHxxBBB', d[2:])
+        pm25 = r[0] / 10.0
+        pm10 = r[1] / 10.0
+        checksum = sum(ord(v) for v in d[2:8]) % 256
+        return (pm25, pm10, checksum)
+
+    def run(self):
+        while byte != "\xaa":
+            byte = ser.read(size=1)
+        d = ser.read(size=10)
+        if d[0] == "\xc0":
+            self.process_frame(byte + d)
---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from SDS011 import SDS011
+import time
+
+import Adafruit_CharLCD as LCD
+
+# Raspberry Pi pin configuration:
+lcd_rs = 26
+lcd_en = 19
+lcd_d4 = 13
+lcd_d5 = 6
+lcd_d6 = 5
+lcd_d7 = 11
+lcd_backlight = 4
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows    = 2
+
+# Initialize the LCD using the pins above.
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+
+if __name__ == "__main__":
+    pm25, pm10, crc = SDS011.run()
+    # Print a two line message
+    lcd.message("PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
+    time.sleep(5)
+    lcd.clear()
